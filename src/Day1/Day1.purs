1fbbc62ed18d5d70407dfc3d115eb6bbf4296cf8
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2024-12-01 10:01:34 -0500

    d1p1

---
+++ src/Day1/Day1.purs
@@ -0,0 +1,40 @@
+module Day1 where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Foldable (sum)
+import Data.Function (on)
+import Data.List (List, filter, length, sort, unzip, zipWith)
+import Data.Maybe (Maybe(..))
+import Data.Ord (abs)
+import Data.Tuple (Tuple(..))
+import Debug (spyWith)
+import Effect (Effect)
+import Main (run)
+import Parsing (Parser)
+import Parsing.Combinators (sepBy)
+import Parsing.String (string)
+import Parsing.String.Basic (intDecimal, skipSpaces)
+import Partial (crashWith)
+
+main ∷ Effect Unit
+main = run 1 Nothing parser (Right <<< calc)
+
+calc :: (List (Tuple Int Int)) -> Int
+calc rows =
+  let
+    Tuple l1 l2 = unzip rows
+    absDiff n1 n2 = abs (n1 - n2)
+  in
+    zipWith absDiff (sort l1) (sort l2)
+      # sum
+
+parser ∷ Parser String (List (Tuple Int Int))
+parser = row `sepBy` (string "\n")
+
+row = do
+  n1 <- intDecimal
+  skipSpaces
+  n2 <- intDecimal
+  pure $ Tuple n1 n2
