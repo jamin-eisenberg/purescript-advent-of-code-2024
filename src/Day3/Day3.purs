5a1b8031dc6ea1f7fdd9f9827595f3e99276cbb1
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2024-12-03 08:16:47 -0500

    d3p2

--- src/Day3/Day3.purs
+++ src/Day3/Day3.purs
@@ -3,15 +3,17 @@ module Day3 where
 import Prelude
 
 import Data.Either (Either(..))
-import Data.Foldable (sum)
-import Data.List (List)
+import Data.Foldable (foldl, foldr, sum)
+import Data.Generic.Rep (class Generic)
+import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..))
+import Data.Show.Generic (genericShow)
 import Data.Tuple (Tuple(..), snd, uncurry)
 import Debug (spyWith)
 import Effect (Effect)
 import Main (run)
 import Parsing (Parser)
-import Parsing.Combinators (many, manyTill_, sepEndBy, try)
+import Parsing.Combinators (choice, many, manyTill_, sepEndBy, try)
 import Parsing.String (anyChar, anyTill, char, satisfy, string)
 import Parsing.String.Basic (intDecimal)
 import Partial (crashWith)
@@ -19,16 +21,28 @@ import Partial (crashWith)
 main ∷ Effect Unit
 main = run 3 Nothing parser (Right <<< calc)
 
-calc :: List (Tuple Int Int) -> Int
-calc muls =
-  muls
+calc :: List Term -> Int
+calc terms =
+  terms
+    # foldl handleTerm { validMuls: Nil, enabled: true }
+    # _.validMuls
     <#> uncurry mul
     # sum
 
-parser :: Parser String (List (Tuple Int Int))
+handleTerm { validMuls, enabled } =
+  case _ of
+    Do -> { validMuls, enabled: true }
+    Don't -> { validMuls, enabled: false }
+    Mul n1 n2 -> if enabled then { validMuls: Tuple n1 n2 : validMuls, enabled } else { validMuls, enabled }
+
+data Term = Do | Don't | Mul Int Int
+
+parser :: Parser String (List Term)
 parser = do
-  muls <- many $ try $ anyTill mulParser -- (anyChar `manyTill_` mulParser)
+  muls <- many $ try $ anyTill termParser -- (anyChar `manyTill_` mulParser)
   pure (muls <#> snd)
+
+termParser = choice [ mulParser, string "do()" $> Do, string "don't()" $> Don't ]
 
 mulParser = do
   _ <- string "mul("
@@ -36,4 +50,4 @@ mulParser = do
   _ <- char ','
   n2 <- intDecimal
   _ <- char ')'
-  pure $ Tuple n1 n2
+  pure $ Mul n1 n2
