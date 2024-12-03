5a69b01f42465f2b7665b0f4383e3170cf9992ef
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2024-12-03 08:19:13 -0500

    gen_day shouldn't overwrite

--- gen_day.bash
+++ gen_day.bash
@@ -1,4 +1,8 @@
-mkdir "./src/Day${1}"
-sed "s/DAY_NUM/${1}/g" ./DayX.purs > "./src/Day${1}/Day${1}.purs"
-touch "./src/Day${1}/sample.txt"
-touch "./src/Day${1}/real.txt"
+if [ ! -e "./src/Day${1}" ]; then
+    mkdir "./src/Day${1}"
+    sed "s/DAY_NUM/${1}/g" ./DayX.purs > "./src/Day${1}/Day${1}.purs"
+    touch "./src/Day${1}/sample.txt"
+    touch "./src/Day${1}/real.txt"
+else
+    echo "./src/Day${1} already exists. Skipping generation."
+fi
