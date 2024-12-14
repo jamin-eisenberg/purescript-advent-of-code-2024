13eec31b2a6417af7b5ceb1e96a2b679ead67e51
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2024-12-14 16:38:32 -0500

    d14p1

--- src/Day14/Day14.purs
+++ src/Day14/Day14.purs
@@ -2,19 +2,52 @@ module Day14 where
 
 import Prelude
 
+import Data.Either (Either(..))
+import Data.Foldable (product)
+import Data.Function (applyN)
+import Data.List (List, filter, length)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Debug (spyWith)
 import Effect (Effect)
 import Main (run)
+import Parsing.Combinators (sepBy)
+import Parsing.String (char, string)
+import Parsing.String.Basic (intDecimal)
 import Partial (crashWith)
-import Data.Maybe (Maybe(..))
-import Debug (spyWith)
 
 main ∷ Effect Unit
-main = run 14 Nothing parser calc
+main = run 14 Nothing parser (Right <<< calc)
 
-calc lines =
+width = 101
+height = 103
+
+calc ∷ List Robot → Int
+calc robots =
   let
-    _ = spyWith "" show lines
+    finalRobots = robots <#> applyN move 100 <#> _.pos
+    quad (Tuple compX compY) = finalRobots # filter (\{ x, y } -> (compX x (width / 2)) && compY y (height / 2))
+    quadrantsComps = [ Tuple (<) (<), Tuple (<) (>), Tuple (>) (<), Tuple (>) (>) ]
   in
-    crashWith "calc not implemented"
+    quadrantsComps <#> quad <#> (spyWith "" show) <#> length # product
 
-parser = crashWith "parser not implemented"
+type Robot = { pos :: { x :: Int, y :: Int }, vel :: { x :: Int, y :: Int } }
+
+move :: Robot -> Robot
+move robot@{ pos, vel } = robot
+  { pos =
+      { x: (pos.x + vel.x) `mod` width, y: (pos.y + vel.y) `mod` height }
+  }
+
+parser = robot `sepBy` char '\n'
+
+robot = do
+  _ <- string "p="
+  px <- intDecimal
+  _ <- char ','
+  py <- intDecimal
+  _ <- string " v="
+  vx <- intDecimal
+  _ <- char ','
+  vy <- intDecimal
+  pure { pos: { x: px, y: py }, vel: { x: vx, y: vy } }
