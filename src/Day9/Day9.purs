b4b46af2beb1f2c3328e1d6b58feef6a44bb8b2b
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2024-12-18 07:48:14 -0500

    d9p1

--- src/Day9/Day9.purs
+++ src/Day9/Day9.purs
@@ -5,17 +5,19 @@ import Prelude
 import Control.Monad.Free (Free)
 import Control.Monad.ST (while)
 import Control.Monad.ST as ST
-import Control.Monad.ST.Internal (new, read)
-import Data.Array (concat, cons, filter, findIndex, length, mapWithIndex, partition, zip)
-import Data.Array.ST (thaw)
+import Control.Monad.ST.Internal (modify, new, read, write)
+import Data.Array (catMaybes, concat, cons, filter, findIndex, findLastIndex, length, mapWithIndex, partition, replicate, zip)
+import Data.Array.ST (STArray, freeze, thaw)
 import Data.Array.ST as STArray
 import Data.Char (fromCharCode, toCharCode)
 import Data.Either (Either(..))
+import Data.Foldable (sum)
 import Data.Function (on)
 import Data.Generic.Rep (class Generic)
-import Data.Int (even)
-import Data.Maybe (Maybe(..))
+import Data.Int (even, toNumber)
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Show.Generic (genericShow)
+import Data.String (joinWith)
 import Data.String.CodeUnits (toCharArray)
 import Data.Tuple (Tuple(..), curry, fst, snd, uncurry)
 import Debug (spyWith)
@@ -32,32 +34,71 @@ main = run 9 Nothing parser (Right <<< calc)
 calc str =
   let
     digits = str # toCharArray <#> toCharCode <#> (_ - toCharCode '0')
-    blocks = spyWith "" show $ getBlocks digits
+    blocks = getBlocks digits # concat
+    condensed = condense blocks
   in
-    1
+    ( ( condensed
+          <#>
+            ( case _ of
+                Free -> Nothing
+                File id -> Just id
+            )
+      )
+        # catMaybes
+        <#> toNumber
+    )
+      `flip mapWithIndex` (\i n -> toNumber i * n)
+      # sum
 
 getBlocks =
-  mapWithIndex (\i blockSize -> if even i then File i blockSize else Free blockSize)
+  mapWithIndex
+    ( \i blockSize ->
+        if even i then
+          replicate blockSize $ File $ i / 2
+        else replicate blockSize Free
+    )
 
 condense :: Array Block -> Array Block
-condense blocks =
-  []
+condense immutableBlocks =
+  ST.run
+    do
+      blocks <- thaw immutableBlocks
+      pointerStart <- new 0
+      pointerEnd <- new $ length immutableBlocks - 1
+      while ((<) <$> read pointerStart <*> read pointerEnd) do
+        currPointerStart <- read pointerStart
+        currPointerEnd <- read pointerEnd
+        currBlock <- STArray.peek currPointerStart blocks
+        if currBlock == Just Free then do
+          swap currPointerStart currPointerEnd blocks
+          currBlocks <- freeze blocks
+          let lastNonFree = findLastIndex (notEq Free) currBlocks # fromMaybe (-1)
+          void $ write lastNonFree pointerEnd
+        else
+          pure unit
+        modify (_ + 1) pointerStart
+      freeze blocks
 
--- ST.run
---   do
---     blocks <- thaw immutableBlocks
---     pointerStart <- new 0
---     pointerEnd <- new $ length immutableBlocks
---     while ((>) <$> read pointerStart <*> read pointerEnd) do
---       currPointerStart <- read pointerStart
---       currPointerEnd <- read pointerEnd
---     pure []
+swap :: forall h a. Int -> Int -> STArray h a -> ST.ST h Unit
+swap i1 i2 arr =
+  do
+    e1' <- STArray.peek i1 arr
+    e2' <- STArray.peek i2 arr
+    case e1', e2' of
+      Just e1, Just e2 ->
+        do
+          _ <- STArray.poke i1 e2 arr
+          void $ STArray.poke i2 e1 arr
+      _, _ -> pure unit
+
+instance Show Block where
+  show = case _ of
+    Free -> "."
+    File id -> show id
 
 type Id = Int
-data Block = File Id Int | Free Int
+data Block = File Id | Free
 
-derive instance Generic Block _
-instance Show Block where
-  show = genericShow
+derive instance Eq Block
 
 parser = rest
